59e2cc95cf4e33c0b754819042cbe551511886cd
Marcin Wojtas <wojtasmarcin.20@gmail.com>
2026-05-06 02:19:59 +0200

    Update API endpoint in app

--- inpost-frontend/src/App.tsx
+++ inpost-frontend/src/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
       setUserPos({ lat, lng });
 
       try {
-        const API_URL = import.meta.env.VITE_API_URL;
+        const API_URL = "http://localhost:3001";
 
         const res = await fetch(`${API_URL}/api/points`);
         const data: Locker[] = await res.json();
